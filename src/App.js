9d3b41c8ee8e35272f71d12ab30a0ea02b3ad956
Maurici093 <bryamvaron@gmail.com>
2022-03-10 23:27:29 -0500

    add more componets and menu functions

--- src/App.js
+++ src/App.js
@@ -3,21 +3,24 @@ import { Route, Switch} from 'react-router-dom';
 import AllMeetupsPage from './pages/AllMeetups';
 import FavoritesPage from './pages/Favorites';
 import NewMeetupsPage from './pages/NewMeetups';
+import Layout from './components/layout/Layout';
 
 function App() {
   return (
     <div>
-      <Switch>
-        <Route path='/' exact>
-          <AllMeetupsPage />
-        </Route>
-        <Route path='/new-meetup'>
-          <NewMeetupsPage />
-        </Route>
-        <Route path='/favorites'>
-          <FavoritesPage />
-        </Route>
-      </Switch>
+      <Layout>
+        <Switch>
+          <Route path='/' exact>
+            <AllMeetupsPage />
+          </Route>
+          <Route path='/new-meetup'>
+            <NewMeetupsPage />
+          </Route>
+          <Route path='/favorites'>
+            <FavoritesPage />
+          </Route>
+        </Switch>
+      </Layout>
     </div>
   );
 }
